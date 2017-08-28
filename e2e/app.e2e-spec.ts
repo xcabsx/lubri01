152926974c930dcfbaa1cded423cc616c170a806
cristian <xcabsx@gmail.com>
2017-08-27 22:23:06 -0300

    commit localhost 2708

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('angular App', () => {
-  let page: AngularPage;
+describe('lubri01 App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
