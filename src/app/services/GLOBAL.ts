301823ee15898175cce712cf7eb6af0a4db0b1a3
xcabsx <31257222+xcabsx@users.noreply.github.com>
2017-08-28 08:24:20 -0300

    Update GLOBAL.ts

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,3 +1,4 @@
 export const GLOBAL = {
-  url: 'http://localhost/curso-backfront/symfony/web/app_dev.php'
+  url: 'http://localhost/curso-backfront/symfony/web/app_dev.php',
+  url2: 'http://localhost/cursoangular4-back/index.php/'
 };
