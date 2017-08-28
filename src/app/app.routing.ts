9c2184d9008d1a333080b5be0407422e5e9523a9
cristian <xcabsx@gmail.com>
2017-08-28 08:18:26 -0300

    commit localhost 2708

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,6 +6,10 @@ import { LoginComponent} from './components/login.component';
 import { RegisterComponent} from './components/register.component';
 import { DefaultComponent} from './components/default.component';
 import { UserEditComponent} from './components/user.edit.component';
+import {ProductosListComponent} from './components/productos-list.component';
+import  { ProductoAddComponent} from './components/productos-add.component';
+import { ProductoDetailComponent} from './components/producto-detail.component';
+import  { ProductoEditComponent} from './components/producto-edit.component';
 
 const appRoutes: Routes =[
   {path: '' , component: DefaultComponent},
@@ -14,6 +18,10 @@ const appRoutes: Routes =[
   {path: 'login/:id' , component: LoginComponent},
   {path: 'register' , component: RegisterComponent},
   {path: 'user-edit', component: UserEditComponent},
+  {path: 'productos', component: ProductosListComponent},
+  { path:'productos-add', component: ProductoAddComponent},
+  { path:'producto/:id', component: ProductoDetailComponent},
+  { path:'editar-producto/:id', component: ProductoEditComponent},
   {path: '**' , component: LoginComponent}
 ];
 
